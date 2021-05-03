8dd5829b2f4da1e714cadc05926703878fe70995
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-03 10:12:35 +0900

    add bioword2vec

--- generate_vectors.py
+++ generate_vectors.py
@@ -141,21 +141,21 @@ def encode_with_models(datasets, models_to_use, save_folder):
 def main():
     folder = ROOT_FOLDER
     save_folder = SAVE_FOLDER
-    size = 100
+    size = None
     models_to_use = [x[2] for x in MODELS]
     datasets = utils.get_sentence_datasets_from_folder(folder, size=size, file_name="ent_train.tsv")
 
     for n, d in datasets:
         print("{} size {}".format(n, len(d)))
 
-    # model_to_domain_to_encodings = encode_with_models(datasets, models_to_use, save_folder)
-    # print("Model keys: {}".format(model_to_domain_to_encodings.keys()))
+    model_to_domain_to_encodings = encode_with_models(datasets, models_to_use, save_folder)
+    print("Model keys: {}".format(model_to_domain_to_encodings.keys()))
 
     dataset_to_states = encode_with_bioword2vec(datasets, save_folder)
     print("BioWordVec keys: {}".format(dataset_to_states.keys()))
 
-    # model_to_domain_to_encodings.update(dataset_to_states)
-    # print("Model keys: {}".format(model_to_domain_to_encodings.keys()))
+    model_to_domain_to_encodings.update(dataset_to_states)
+    print("Model keys: {}".format(model_to_domain_to_encodings.keys()))
 
 
 if __name__ == "__main__":
