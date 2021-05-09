39d26f237076d04ba4f4b735b63327214a43d632
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-09 21:44:50 +0900

    a

--- train_nermodels.py
+++ train_nermodels.py
@@ -37,7 +37,7 @@ CONLL_SAVE_PATH = "conll_output_0505.txt"
 BioWordVec_FOLDER = "../biobert_data/bio_embedding_extrinsic"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # dataset_list = ['s800', 'NCBI-disease', 'JNLPBA', 'linnaeus', 'BC4CHEMD', 'BC2GM', 'BC5CDR', 'conll-eng']
-dataset_list = ["BC4CHEMD"]
+dataset_list = ['BC4CHEMD', 'BC2GM', 'BC5CDR', 'conll-eng']
 
 # train_file_path = "/Users/ardaakdemir/bioMLT_folder/biobert_data/datasets/BioNER_2804/BC2GM/ent_train.tsv"
 # dev_file_path = "/Users/ardaakdemir/bioMLT_folder/biobert_data/datasets/BioNER_2804/BC2GM/ent_devel.tsv"
