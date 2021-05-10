3285994109b428b31ea604aac762557c809303aa
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-10 13:23:05 +0900

    a

--- copy_devtest.py
+++ copy_devtest.py
@@ -1,5 +1,5 @@
 import os
-
+import subprocess
 file_names = ["ent_devel.tsv","ent_test.tsv"]
 
 
@@ -9,3 +9,12 @@ dataset_list = ['s800', 'NCBI-disease', 'JNLPBA', 'linnaeus', 'BC4CHEMD', 'BC2GM
 model_list = ['robertaLarge', 'distilbertBaseUncased', 'BioBERT', 'BioWordVec']
 for model in model_list:
     my_folder = os.path.join(save_folder_root,model)
+    for d in dataset_list:
+        src = os.path.join(src_folder_root,d)
+        dest = os.path.join(my_folder,d)
+        if not os.path.isdir(dest):os.makedirs(dest)
+        for f in file_names:
+            s = os.path.join(src,f)
+            d = os.path.join(dest,f)
+            cmd = "cp {} {}".format(s,d)
+            subprocess.call(cmd,shell=True)
