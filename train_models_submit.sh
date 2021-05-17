28b09abd8b28438dcc6ce0425fc18dd6035a8605
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-17 23:24:20 +0900

    a

--- train_models_submit.sh
+++ train_models_submit.sh
@@ -3,6 +3,6 @@
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
 cd ~/ner-data-selection
 
-save_folder="../selection_labeled_1005_randoms"
-dataset_root="../dataselection_1005_labeled"
+save_folder="../subsetselection_labeled_1705"
+dataset_root="../dataselection_1705_labeled"
 singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/train_nermodels.py --multiple --multi_model --save_folder_root ${save_folder} --dataset_root ${dataset_root}  --evaluate_root ${dataset_root}
