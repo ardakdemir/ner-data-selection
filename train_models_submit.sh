c819289c4fa8ff570abd2270520d59f4ebef236b
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-17 15:05:05 +0900

    a

--- train_models_submit.sh
+++ train_models_submit.sh
@@ -2,6 +2,8 @@
 #$ -cwd
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
 #$ -N generate_vectors
-
 cd ~/ner-data-selection
-singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/generate_vectors.py
+
+save_folder="../selection_labeled_1005_randoms"
+dataset_root="../dataselection_1005_labeled"
+singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/train_nermodels.py --multiple --multi_model --save_folder_root ${save_folder} --dataset_root ${dataset_root}  --evaluate_root ${dataset_root}
