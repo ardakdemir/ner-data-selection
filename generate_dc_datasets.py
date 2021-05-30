662c4963b618556ebfc18055d387ade93c6ca3a1
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-30 16:54:31 +0900

    a

--- generate_dc_datasets.py
+++ generate_dc_datasets.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import json
 
-ROOT_FOLDER = "/Users/ardaakdemir/bioMLT_folder/biobert_data/datasets/BioNER_2804_labeled_cleaned"
+ROOT_FOLDER = "/Users/aakdemir/biobert_data/datasets/BioNER_2804_labeled_cleaned"
 
 bio_datasets = ['s800', 'NCBI-disease', 'JNLPBA', 'linnaeus', 'BC4CHEMD', 'BC2GM', 'BC5CDR']
 news_datasets = ["conll-eng"]
@@ -18,7 +18,7 @@ def get_out_domain_data(domain_datasets, indomain_name, size):
     for x in domain_datasets:
         if x == indomain_name: continue
         all_od_datasets.extend(domain_datasets[x]["train"])
-    print("{} out of domain datasets in total. ".format(len(all_od_datasets)))
+    print("Out of domain {} datasets in total... ".format(len(all_od_datasets)))
     np.random.shuffle(all_od_datasets)
     all_od_datasets = all_od_datasets[:size]
     return all_od_datasets
@@ -36,35 +36,37 @@ def get_dc_datasets(ROOT_FOLDER, split=0.80, oov_rel_size=5):
                 sentences = [" ".join([t.split()[0] for t in sent.split("\n") if len(sent.split("\n")) > 0]) for sent in
                              dataset]
                 domain_datasets[d][k] = sentences
-
-    for in_domain in all_datasets:
+    print("Domain dataset keys: {}".format(domain_datasets.keys()))
+    for in_domain_name in all_datasets:
         o_label = "OTHER_DOMAIN"
-        for d in in_domain:
-            in_domain = domain_datasets[d]["dev"]
-            in_domain_size = len(in_domain)
-            print("{} indomain sentences for {}".format(in_domain_size, d))
-            out_domain_size = oov_rel_size * in_domain_size
-            out_domain_dataset = get_out_domain_data(domain_datasets, indomain_name, size)
-            np.random.shuffle(out_domain_dataset)
-            out_domain_dataset = out_domain_dataset[:out_domain_size]
-            print("OOD size: {}".format(len(out_domain_dataset)))
-            dc_dataset = in_domain + out_domain_dataset
-            labels = [d for _ in range(len(in_domain))] + [o_label for _ in range(len(out_domain_dataset))]
-            dc_dataset = list(zip(dc_dataset, labels))
-            np.random.shuffle(dc_dataset)
-            split_index = int(len(dc_dataset) * split)
-            sentences, labels = list(zip(*dc_dataset))
-            dc_datasets[d] = {"train": {"sentences": sentences[:split_index], "labels": labels[:split_index]},
-                              "test": {"sentences": sentences[split_index:], "labels": labels[split_index:]}}
-            save_folder = os.path.join(SAVE_FOLDER, d)
-            if not os.path.isdir(save_folder):
-                os.makedirs(save_folder)
-            for x, data in dc_datasets[d].items():
-                save_path = os.path.join(save_folder, "{}.json".format(x))
-                with open(save_path, "w") as j:
-                    json.dump(data, j)
+        in_domain_dataset = domain_datasets[d]["dev"]
+        in_domain_size = len(in_domain_dataset)
+        print("{} indomain sentences for {}".format(in_domain_size, in_domain_name))
+        out_domain_size = oov_rel_size * in_domain_size
+        out_domain_dataset = get_out_domain_data(domain_datasets, in_domain_name, out_domain_size)
+        np.random.shuffle(out_domain_dataset)
+        out_domain_dataset = out_domain_dataset[:out_domain_size]
+        print("OOD size: {}".format(len(out_domain_dataset)))
+        dc_dataset = in_domain_dataset + out_domain_dataset
+        labels = [in_domain_name for _ in range(len(in_domain_dataset))] + [o_label for _ in
+                                                                            range(len(out_domain_dataset))]
+        dc_dataset = list(zip(dc_dataset, labels))
+        np.random.shuffle(dc_dataset)
+        split_index = int(len(dc_dataset) * split)
+        sentences, labels = list(zip(*dc_dataset))
+        dc_datasets[in_domain_name] = {
+            "train": {"sentences": sentences[:split_index], "labels": labels[:split_index]},
+            "test": {"sentences": sentences[split_index:], "labels": labels[split_index:]}}
+        save_folder = os.path.join(SAVE_FOLDER, in_domain_name)
+        if not os.path.isdir(save_folder):
+            os.makedirs(save_folder)
+        for x, data in dc_datasets[in_domain_name].items():
+            save_path = os.path.join(save_folder, "{}.json".format(x))
+            with open(save_path, "w") as j:
+                json.dump(data, j)
 
 
-for rel_size in [1]:
-    SAVE_FOLDER = "/Users/ardaakdemir/bioMLT_folder/biobert_data/datasets/BioNER_2505_DC_datasets_relsize_{}".format(rel_size)
-    get_dc_datasets(ROOT_FOLDER, split=0.80,oov_rel_size=rel_size)
+for rel_size in [1,2,3,5,10]:
+    SAVE_FOLDER = "/Users/aakdemir/biobert_data/datasets/BioNER_2505_DC_datasets_relsize_{}".format(
+        rel_size)
+    get_dc_datasets(ROOT_FOLDER, split=0.80, oov_rel_size=rel_size)
