55ee9bff7097141fb6ffe5bdba39ae888c18f2fc
Arda Akdemir <ardaakdemir1@gmail.com>
2021-05-31 09:16:21 +0900

    a

--- traindc_trainner_submitter.sh
+++ traindc_trainner_submitter.sh
@@ -7,7 +7,7 @@ do
   dc_save_folder="../dc_result_3105_"${i}
   ner_save_folder="../ner_result_3105_"${i}
   dc_dataset_root="../biobert_data/datasets/BioNER_2505_DC_datasets_relsize_"${i}
-  ner_dataset_root="../instanceselection_2005_labeled"
+  ner_dataset_root="../instanceselection_2005_labeled/BioBERT"
   exp_name="BioNER_2505_DC_datasets_relsize_"${i}
   qsub -N $exp_name traindc_trainner_submit.sh ${dc_save_folder} ${ner_save_folder} ${dc_dataset_root} ${ner_dataset_root}
 done
