01760d99ebcf86c66314a7cfcc842c1e52155c97
Arda Akdemir <ardaakdemir1@gmail.com>
2021-06-06 13:36:50 +0900

    a

--- traindc_trainner_submit.sh
+++ traindc_trainner_submit.sh
@@ -10,4 +10,4 @@ ner_dataset_root=${4}
 
 echo $dc_save_folder"  "$ner_save_folder"  "$dc_dataset_root"  "$ner_dataset_root
 #singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/train_dc_models.py --save_folder_root ${dc_save_folder} --save_folder ${dc_save_folder} --dataset_root ${dc_dataset_root}  --evaluate_root ${dc_dataset_root} --multiple
-singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/train_nermodels.py --dc_save_root ${dc_save_folder} --save_folder_root ${ner_save_folder} --save_folder ${ner_save_folder} --dataset_root ${ner_dataset_root}  --evaluate_root ${ner_dataset_root} --load_dc_model  --multiple
+singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/train_nermodels.py --dc_save_root ${dc_save_folder} --save_folder_root ${ner_save_folder} --save_folder ${ner_save_folder} --dataset_root ${ner_dataset_root}  --evaluate_root ${ner_dataset_root} --load_dc_model  --multiple --dev_only
