5c0367d9bf66a8bfb64d6d11ae0d0ddf5a133dfc
Arda Akdemir <ardaakdemir1@gmail.com>
2021-06-06 14:47:50 +0900

    a

--- generate_vector_submit.sh
+++ generate_vector_submit.sh
@@ -1,7 +1,7 @@
 /usr/local/bin/nosh
 #$ -cwd
 #$ -l os7,v100=1,s_vmem=100G,mem_req=100G
-#$ -N dataselection_pipeline
+#$ -N generate_vectors
 
 cd ~/ner-data-selection
 #singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python ~/ner-data-selection/dataselection_pipeline.py
