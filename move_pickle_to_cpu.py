cbb7fc27b66bf0bcb0968256ab8bf8c659c78d6e
Arda Akdemir <ardaakdemir1@gmail.com>
2022-01-23 09:53:18 +0900

    move pickle to cpu

--- move_pickle_to_cpu.py
+++ move_pickle_to_cpu.py
@@ -1,6 +1,7 @@
 import pickle as pkl
 from collections import defaultdict
 import torch
+import os
 
 p = "../bioMLT_folder/entity_vectors_2201/entity_vectors_5000_entities.pkl"
 save_path = os.path.join(os.path.split(p)[0],os.path.split(p)[1].split(".")[0]+"_cpu.pkl")
@@ -20,4 +21,4 @@ for k, d in entity_dict.items():
             new_dict[k][name][ent] = torch.mean(torch.stack(vecs),dim=0).detach().cpu().numpy()
 
 with open(save_path, "wb") as p:
-    pickle.dump(entity_vector_dict, p)
+    pkl.dump(entity_vector_dict, p)
