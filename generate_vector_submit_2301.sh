5dd55c7c8d0ae34dbcc19bb1208b8d327cf37150
Arda Akdemir <ardaakdemir1@gmail.com>
2022-01-24 09:14:14 +0900

    generate for all aux sizes

--- generate_vector_submit_2301.sh
+++ generate_vector_submit_2301.sh
@@ -4,7 +4,8 @@
 #$ -N generate_vectors
 dataset_name=${1}
 save_folder=${2}
+aux_size=${3}
 size=3000
 cd ~/ner-data-selection
-singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python3 generate_vectors.py --model_load_path ../dc_result_3105_5/${dataset_name}/best_model_weights.pkh  --save_folder ${save_folder}
+singularity exec  --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python3 generate_vectors.py --model_load_path ../dc_result_3105_${aux_size}/${dataset_name}/best_model_weights.pkh  --save_folder ${save_folder}
 
--- generate_vector_submitter.sh
+++ generate_vector_submitter.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-save_folder="dc_vectors_2301/"
+save_folder="../dc_vectors_2401"
 for dataset_name in "BC2GM" "BC5CDR" "JNLPBA" 'NCBI-disease' "BC4CHEMD" 'conll-eng'
 do
-  my_save_folder=${save_folder}${dataset_name}
-  exp_name="vectorgeneration_2301_"${dataset_name}
-  echo ${my_save_folder}"  "${dataset_name}
-  qsub -N $exp_name generate_vector_submit_2301.sh ${dataset_name} ${my_save_folder}
+  for aux_size in 1 2 3 5 10
+  do
+    my_save_folder=${save_folder}"_"${aux_size}"/"${dataset_name}
+    exp_name="vectorgeneration_2401_"${dataset_name}
+    echo ${my_save_folder}"  "${dataset_name}
+    qsub -N $exp_name generate_vector_submit_2301.sh ${dataset_name} ${my_save_folder} ${aux_size}
+  done
+
 done
